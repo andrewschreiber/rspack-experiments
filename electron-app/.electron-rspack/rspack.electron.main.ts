86e6ef06234e1bf7546bd6555e7b21a2e4a9019e
Andrew Schreiber <andrew.schreiber1@gmail.com>
2024-10-11 22:13:11 -0400

    feat: import * to solve commonjs issue, but now load is slow

--- electron-app/.electron-rspack/rspack.electron.main.ts
+++ electron-app/.electron-rspack/rspack.electron.main.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from '@rspack/cli'
-import path from 'path'
+import * as path from 'path'
 import { spawn } from 'child_process'
 import { RspackOptions } from '@rspack/core'
 
